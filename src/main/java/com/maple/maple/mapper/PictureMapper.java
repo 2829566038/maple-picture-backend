13c8f73314c378f05c3c0c4e8b39d8cc6b408aec
maple <2829566038@qq.com>
2026-05-09 11:18:11 +0800

    测试接口

---
+++ src/main/java/com/maple/maple/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.maple.maple.mapper;
+
+import com.maple.maple.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author A
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2026-05-09 11:14:10
+* @Entity com.maple.maple.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/PictureService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import com.maple.maple.model.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author A
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2026-05-09 11:14:10
+*/
+public interface PictureService extends IService<Picture> {
+
+}
